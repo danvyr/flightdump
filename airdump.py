e536d2627bb8d54741cfff6fca6524e890206a25
Vitali Danilovich <danvyr@gmail.com>
2019-05-07 23:02:31 +0300

    add py

--- airdump.py
+++ airdump.py
@@ -4,6 +4,14 @@
 
 
 import urllib.request, json 
+import datetime
+
+
+
 with urllib.request.urlopen("http://192.168.2.111/dump1090-fa/data/aircraft.json") as url:
     data = json.loads(url.read().decode())
-    print(data)
+    print(data['now'])
+    s = float(data['now'])
+    print (datetime.datetime.fromtimestamp(s).strftime('%Y-%m-%d %H:%M:%S.%f'))
+
+ 
